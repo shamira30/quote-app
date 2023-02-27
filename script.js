e82d0bc55736098ea0944ad9c791976b5ea2d131
shamira30 <shamila3011@googlemail.com>
2023-02-27 23:30:01 +0000

    added response data to ajax

--- script.js
+++ script.js
@@ -10,7 +10,19 @@ $.ajax ({
         var results = response.data;
 
         for (var i = 0; i < results.length; i++) {
-            var
+            var gifDiv = $("<div>");
+
+            var rating = results [i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var personImage = $("<img>");
+            personImage.attr("src", results [i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+
+            $("#gifs-appear-here").prepend(gifDiv);
         }
-    })
-})
+    });
+});
