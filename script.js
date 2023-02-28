939a88d54cf53d01985e79393eb11ec69432ffe1
shamira30 <shamila3011@googlemail.com>
2023-02-28 19:27:16 +0000

    added div classes

--- script.js
+++ script.js
@@ -26,3 +26,51 @@ $.ajax ({
         }
     });
 });
+
+function get_quote_of_the_day() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+	 if (this.readyState == 4 && this.status == 200) {
+	     // Access the result here
+	     alert(this.responseText);
+	 }
+    };
+    xhttp.open("GET", "https://quotes.rest/qod?category=inspire", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("X-Theysaidso-Api-Secret", "YOUR API HERE");
+    xhttp.send();
+}
+
+get_quote_of_the_day()
+
+{
+    "success": {
+      "total": 1
+    },
+    "contents": {
+      "quotes": [
+        {
+          "quote": "If you respect yourself in stressful situations, it will help you see the positiveâ€¦ It will help you see the message in the mess.",
+          "length": "135",
+          "author": "Steve Maraboli",
+          "tags": [
+            "inspire",
+            "self-respect",
+            "stress"
+          ],
+          "category": "inspire",
+          "language": "en",
+          "date": "2020-02-02",
+          "permalink": "https://theysaidso.com/quote/steve-maraboli-if-you-respect-yourself-in-stressful-situations-it-will-help-you",
+          "id": "nwW3g7V0xszGDNIehz6yTgeF",
+          "background": "https://theysaidso.com/img/bgs/man_on_the_mountain.jpg",
+          "title": "Inspiring Quote of the day"
+        }
+      ]
+    },
+    "baseurl": "https://theysaidso.com",
+    "copyright": {
+      "year": 2022,
+      "url": "https://theysaidso.com"
+    }
+  }
